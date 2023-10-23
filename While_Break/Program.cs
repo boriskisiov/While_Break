80f559db35fd74393d8516eeedc58efc63fd3cf6
boriskisiov <boriskisiov@gmail.com>
2023-10-23 16:15:53 +0300

    Add project files.

---
+++ While_Break/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace While_Break
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            while (true)
+            {
+                int nextNum = int.Parse(Console.ReadLine());
+                if (nextNum == 0)
+                {
+                    break;
+                }
+                sum += nextNum;
+                Console.WriteLine("sum: " + sum);
+            }
+        }
+    }
+}
